2042490b0805bf600f05ca0ee43d3183d1197eb1
Mohithraj B J <mohithraj.b.j@sap.com>
2021-06-05 16:23:43 +0530

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import NotFound from "../elements/NotFound/NotFound";
 
 class App extends React.Component {
   render() {
+    //Main COmponent
     return (
       <BrowserRouter>
         <React.Fragment>
